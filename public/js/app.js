77e91efbf4977a3081cd8cf8ceab6cf9f5c203de
Ganesh Sanap <ganesh.sanap2326@gmail.com>
2020-09-05 16:35:15 +0530

    Add name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,8 @@
 var socket = io();
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
+
+console.log(name + ' wants to join ' + room);
 
 socket.on('connect', function() {
     console.log('Connected to socket.io server!');
@@ -8,7 +12,10 @@ socket.on('message', function (message) {
     var momentTimestamp = moment.utc(message.timestamp);
     console.log('New message');
     console.log(message.text);
-    jQuery('.messages').append('<p><strong>' + momentTimestamp.local().format('h:mm a') +'</strong> ' + message.text + '</p>')
+
+    var $message = jQuery('.messages');
+    $message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
+    $message.append('<p>' + message.text + '</p>');
 });
 
 // Hanldes submitting of new message
@@ -19,6 +26,7 @@ $form.on('submit', function (event) {
     var message = $form.find('input[name=message]');
 
     socket.emit('message', {
+        name: name,
         text: message.val()
     });
 
